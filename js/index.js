7c3a4212f0f57060f6382cb31b5ec88a56705b50
deepakrvg <76244467+deepakrvg@users.noreply.github.com>
2021-09-21 12:47:01 +0530

    added closing nav functionality

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelector('.nav__link');
+const navLinks = document.querySelectorAll('.nav__link');
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
